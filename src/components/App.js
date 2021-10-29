c49f79825e99f75f04444eff4ec39d73c95aba5e
Sarah Thiery <sarahvthiery@gmail.com>
2021-10-28 17:09:30 -0700

    Add basic homepage component structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ReactLoading from "react-loading";
+import "../App.css";
+
+import Header from "./Header";
+import MainVideo from "./MainVideo";
+import About from "./About";
+import Map from "./Map";
+import Pricing from "./Pricing";
+import Buildings from "./Buildings";
+import BuildingPage from "./BuildingPage";
+import RoomPage from "./RoomPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/">
+            <MainVideo></MainVideo>
+            <About></About>
+            <Map></Map>
+            <Pricing></Pricing>
+            <Buildings></Buildings>
+          </Route>
+          {/* Come back and create shortened names of buildings to use for resources here */}
+          <Route path="/:buildingId">
+            <BuildingPage></BuildingPage>
+          </Route>
+          <Route path="/:roomId">
+            <RoomPage></RoomPage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
